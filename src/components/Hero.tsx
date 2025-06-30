dd03745306a772a10b96ace0368d3e8a0d936d01
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-06-30 13:09:02 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = ({ scrollY }: HeroProps) => {
       >
         {/* Profile Picture */}
         <img 
-          src="https://github.com/SamyakMishra072/Portfolio/blob/main/input.jpeg" 
+          src="https://i.postimg.cc/SRZXWN0D/samyak.jpg" 
           alt="Samyak Mishra" 
           className="w-32 h-32 md:w-40 md:h-40 rounded-full mx-auto mb-6 shadow-lg border-4 border-white/10 object-cover" 
         />
